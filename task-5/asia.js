31713d58e8e1921ed6bb5dd150d8b0b7ad15f969
shadoeee <109212051+shadoeee@users.noreply.github.com>
2023-10-10 23:36:31 +0530

    Update asia.js

--- task-5/asia.js
+++ task-5/asia.js
@@ -1,16 +1,4 @@
-// let XMLHttpRequest = require('xhr2');
 
-// let xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://restcountries.com/v3.1/all');
-
-// xhr.onload = function () {
-//     let countries = JSON.parse(xhr.responseText);
-
-//     for (let country of countries) {
-
-//     }
-// }
 
 // Fetch the data from the API
 
