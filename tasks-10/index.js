13c2e7a615795239f933caff9d735b9d7c646d03
shadoeee <109212051+shadoeee@users.noreply.github.com>
2024-02-22 19:35:32 +0530

    Update index.js

--- tasks-10/index.js
+++ tasks-10/index.js
@@ -1,59 +1,33 @@
-const hell = document.querySelector(".hell");
+const countdown = document.querySelector(".countdown"); // selected element
 
+let time = 10; // countdown initialization
 
-setTimeout(function() {
-    hell.innerText = "10";
-    setTimeout(function() {
-        hell.innerText = "9";
-        setTimeout(function() {
-
-            hell.innerText = "8";
-            setTimeout(function() {
-
-                hell.innerText = "7";
-                setTimeout(function() {
-
-                    hell.innerText = "6";
-                    setTimeout(function() {
-
-                        hell.innerText = "5";
-                        setTimeout(function() {
-
-                            hell.innerText = "4";
-                            setTimeout(function() {
-
-                                hell.innerText = "3";
-                                setTimeout(function() {
-
-                                    hell.innerText = "2";
-                                    setTimeout(function() {
-
-                                        hell.innerText = "1";
-                                        setTimeout(function() {
-
-                                            hell.innerText = "Happy Independence Day!";
-
-                                        }, 1000);
-
-
-                                    }, 1000);
-
-
+countdown.innerHTML = time--; // callback pain (nested callback)
+setTimeout(() => {
+    countdown.innerHTML = time--;
+    setTimeout(() => {
+        countdown.innerHTML = time--;
+        setTimeout(() => {
+            countdown.innerHTML = time--;
+            setTimeout(() => {
+                countdown.innerHTML = time--;
+                setTimeout(() => {
+                    countdown.innerHTML = time--;
+                    setTimeout(() => {
+                        countdown.innerHTML = time--;
+                        setTimeout(() => {
+                            countdown.innerHTML = time--;
+                            setTimeout(() => {
+                                countdown.innerHTML = time--;
+                                setTimeout(() => {
+                                    countdown.innerHTML = time--;
+                                    countdown.innerHTML = "Happy Independence Day🎉";
                                 }, 1000);
-
-
                             }, 1000);
-
                         }, 1000);
-
                     }, 1000);
-
                 }, 1000);
-
             }, 1000);
-
         }, 1000);
     }, 1000);
 }, 1000);
-
-// console.log("Welcome to loupe.");
